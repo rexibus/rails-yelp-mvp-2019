432a53b828d03e8ea194accc14c652362b2f6905
RegisWeill <regis_weill@hotmail.com>
2019-05-25 16:57:50 +0200

    fix address

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_143111) do
+ActiveRecord::Schema.define(version: 2019_05_25_145615) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
